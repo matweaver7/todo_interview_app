05763172fc95ea64990d8407d3b54fb3f8ea65bb
Brian Armstrong <brian.armstrong@avast.com>
2022-08-03 10:59:18 -0600

    fixing typo in footer

--- src/components/footers/defaultFooter.tsx
+++ src/components/footers/defaultFooter.tsx
@@ -5,7 +5,7 @@ const defaultFooter = () => {
     return (
         <footer className="text-muted">
             <Container>
-                <p className="float-right">This is and example project setup for your convenience.</p>
+                <p className="float-right">This is an example project setup for your convenience.</p>
             </Container>
         </footer>
         
